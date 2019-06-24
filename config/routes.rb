d3147693bbd92bfd7e674099e3d7bb3d40f78dc8
alejovelez10 <alejovelez10@gmail.com>
2019-06-24 14:24:46 -0500

    añadiendo estilos a las tareas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root "tasks#show"
 	post "create_task", to: "tasks#create", as: "create_task" 
+	post "update_task/:id/:state", to: "tasks#update", as: "update_task"
 
 	mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
