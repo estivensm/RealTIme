9bb08bcf6663008706b4db928a75288944d161a9
alejovelez10 <alejovelez10@gmail.com>
2019-06-24 00:38:01 -0500

    subiendo a heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_cable.allowed_request_origins = ['https://estiventime.herokuapp.com', 'http://estiventime.herokuapp.com']
+  config.web_socket_server_url = "wss://estiventime.herokuapp.com/cable" 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
