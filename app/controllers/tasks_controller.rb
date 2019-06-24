2a7f64e9698719226795cc80f281781e13258085
alejovelez10 <alejovelez10@gmail.com>
2019-06-24 11:49:27 -0500

    añadiendo estilos a las tareas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,52 @@
 class TasksController < ApplicationController
+	  before_action :set_task_function, only: [:destroy, :edit, :update]
+
 	def show
 		@tasks = Task.all
 	end
 
 	def create
-		@create_task = Task.create(title: params[:title], body: params[:body], state: params[:state])
+	  	@tasks = Task.create(task_params)
+	  	respond_to do |format|
+	      if @profile_functions.save
+	        format.js
+	      else
+	        format.html { render :new }
+	      end
+	    end
 	end
+
+	  def new
+	    @profile = Profile.find(params[:profile])
+	  end
+
+	  def edit
+	    
+	  end
+
+	  def update
+	    
+	  end
+
+	  def destroy
+	    respond_to do |format|
+	  	  if @profile_functions.destroy
+	        format.js
+	      else 
+	        redirect_to admin_contact_path
+	      end
+	    end 
+	  end
+
+	  private 
+
+	  def set_task_function
+	  	@task = Task.find(params[:id])
+	  end
+
+	  def task_params
+	    params.permit(:title, :user_id, :body, :state)
+	  end
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+	def select_state
+		[
+
+			["Pendiente", "Pendiente"],
+			["Importante", "Importante"],
+			["Realizada", "Realizada"],
+			["No realizada", "No realizada"]
+
+
+		]
+	end
+
+
+	def get_color_state(state)
+		if state == "Pendiente"
+			"border-warning"
+		elsif state == "Importante"
+			"border-danger"
+		elsif state == "Realizada"
+			"border-success"
+		elsif state == "No realizada"
+			"border-info"
+		end
+	end
+
+	def get_button_state(state)
+		if state == "Pendiente"
+			'<button type="button" class="btn btn-outline-warning">Pendiente</button>'
+		elsif state == "Importante"
+			'<button type="button" class="btn btn-outline-danger">Importante</button>'
+		elsif state == "Realizada"
+			'<button type="button" class="btn btn-outline-success">Realizada</button>'
+		elsif state == "No realizada"
+			'<button type="button" class="btn btn-outline-info">No realizada</button>'
+		end
+	end
 end
